eda7abe03942ed21c2cd34b5bd05f435775fc4dc
refscelsi <37337394+refscelsi@users.noreply.github.com>
2018-04-16 18:25:04 +0200

    test2 gitHub

--- Progetto/src/main/java/Progetto/App.java
+++ Progetto/src/main/java/Progetto/App.java
@@ -7,6 +7,6 @@ package Progetto;
 public class App{
     public static void main( String[] args )
     {
-        System.out.println( "Test Github" );
+        System.out.println( "Test Github2" );
     }
 }
