c6e6251483fad7ee4fad6c1d4479814fca3fd913
refscelsi <37337394+refscelsi@users.noreply.github.com>
2018-04-17 15:42:39 +0200

    Inizio lavori

--- Progetto/src/main/java/Progetto/App.java
+++ Progetto/src/main/java/Progetto/App.java
@@ -1,11 +1,13 @@
 package Progetto;
 
-/**
- * Hello world!
- *
- */
+import java.util.*;
 public class App{
     public static void main( String[] args ){
         System.out.println( "Test Github4" );
+        Random r = new Random();
+        int id = r.nextInt(5000);
+        Match m=new Match(id);
+
+        m.startMatch();
     }
 }
---
+++ Progetto/src/main/java/Progetto/Match.java
@@ -0,0 +1,14 @@
+package Progetto;
+
+public class Match {
+
+    private int id;
+
+    public Match(int id) {
+        this.id=id;
+    }
+
+    public void startMatch(){
+        System.out.print("l'id della partita è:"+id);
+    }
+}
