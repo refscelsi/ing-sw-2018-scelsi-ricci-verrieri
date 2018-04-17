b40f9824bc8953c5ae90d53f76f6ee28bb7d5134
arancicarini <10536266@polimi.it>
2018-04-17 14:44:25 +0200

    test github

--- Progetto/src/main/java/Progetto/App.java
+++ Progetto/src/main/java/Progetto/App.java
@@ -3,7 +3,7 @@ package Progetto;
 import java.util.*;
 public class App{
     public static void main( String[] args ){
-        System.out.println( "Test Github4" );
+        System.out.println( "Ciao sono bellissima" );
         Random r = new Random();
         int id = r.nextInt(5000);
         Match m=new Match(id);
