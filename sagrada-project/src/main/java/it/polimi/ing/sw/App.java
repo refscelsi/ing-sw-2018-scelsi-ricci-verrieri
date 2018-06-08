1c425139f8eb8443afdacf5d5ff0dbb6dbd8d44c
refscelsi <10521406@polimi.it>
2018-06-08 22:00:18 +0200

    aggiunto look and feel

--- sagrada-project/src/main/java/it/polimi/ing/sw/App.java
+++ sagrada-project/src/main/java/it/polimi/ing/sw/App.java
@@ -6,55 +6,27 @@ import it.polimi.ing.sw.view.Menu;
 public class App {
     public static Menu menu;
 
-    public static void main( String[] args ){
+    public static void main(String[] args) {
 
-        //System.out.println( "Ciao sono bellissima" );
-        /*Random r = new Random();
-        int id = r.nextInt(5000);
-        int numPlayer=4;
-        Match m=new Match(id,numPlayer);*/
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            //java.util.logging.Logger.getLogger(AddScheme.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            //java.util.logging.Logger.getLogger(AddScheme.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            //java.util.logging.Logger.getLogger(AddScheme.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            //java.util.logging.Logger.getLogger(AddScheme.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
 
-        /*
-        GUI g=new GUI();
-        g.setVisible(true);
-        */
-        //################################### classe vera
-
-        menu =new Menu();
+        menu = new Menu();
         menu.setVisible(true);
-
-        //#################################### cose per testing
-        /*
-        Bag bag= new Bag();
-        bag.setDices();
-        int i=bag.getSize();
-        System.out.println(i);
-
-        System.out.println("Colore"+bag.draw(4));
-        System.out.println(bag.getSize());
-
-        Dice dice= new Dice();
-        System.out.println(dice.throwDice());
-
-        ObjectiveCardDeck deck = new ObjectiveCardDeck();
-        deck.setDeck();
-        deck.drawObjectiveCard(3,true);
-        System.out.println(deck.getDrawnCardsSize());
-
-*/
-        //Match match = new Match(00);
-        //match.inizializePlayer();
-
-        //m.startMatch();
-
-
-
-
-
-
-
-
-
     }
 }
 
